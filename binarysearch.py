c52195de7998ec7e4db724c75cf955ebab732cc9
NerdTsai <nerdtsai@gmail.com>
2015-07-16 16:12:24 +0800

    Create binarysearch.py

---
+++ binarysearch.py
@@ -0,0 +1,18 @@
+# -*- coding: <encoding name> -*-
+list_st = raw_input('Please input a Orderly sequence:')#从小到大顺序
+list_a = eval(list_st)
+aid_val = int(raw_input('Please input number your need:'))
+list_len = len(list_a)
+low = 0
+high = list_len-1
+while low <= high:
+	mid = low +(high-low)/23
+	if aid_val < list_a[mid]:
+		high = mid-1
+	else:
+		if aid_val > list_a[mid]:
+			low = mid+1
+		else:
+			if aid_val == list_a[mid]:
+				print 'The key is:'+str(mid)
+				break			
